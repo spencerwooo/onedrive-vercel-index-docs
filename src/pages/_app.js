95c25a935aa26d2d2ebc4ec4880cb588dac0ab54
spencerwooo <spencer.woo@outlook.com>
2022-01-31 13:25:24 +0800

    google analytics and resolve todo links

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,23 @@
 import '../style.css'
 import 'nextra-theme-docs/style.css'
+import Script from 'next/script'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+      />
+      <Script id="ga-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
