a1a1ea0dad6168bc979bb54da00e00026ffa6b11
spencerwooo <spencer.woo@outlook.com>
2022-02-09 12:44:42 +0800

    add localisation middleware

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   i18n: {
     locales: ['en', 'zh'],
-    defaultLocale: 'en',
+    defaultLocale: 'en'
   },
   reactStrictMode: true
 })
